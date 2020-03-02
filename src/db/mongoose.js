223293c81cf11916cec4a17d616fd099705ade51
Joao Matos Silva <joaomatos.silva@farfetch.com>
2020-03-02 11:28:06 +0000

    updating mongodb connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const MONGO_DB = process.env.MONGO_DB
-mongoose.connect('mongodb://' + MONGO_DB + '/task-manager-api', {
+mongoose.connect(MONGO_DB, {
     useNewUrlParser: true,
     useCreateIndex: true
 })
